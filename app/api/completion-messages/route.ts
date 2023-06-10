1a560d7842a9ee1c6cb817d0add2aacb00eda2ac
Joel <iamjoel007@gmail.com>
2023-06-10 15:04:50 +0800

    feat: support steaming

--- app/api/completion-messages/route.ts
+++ app/api/completion-messages/route.ts
@@ -1,16 +1,13 @@
 import { type NextRequest } from 'next/server'
-import { OpenAIStream } from '@/app/api/utils/stream'
-import { getInfo, client } from '@/app/api/utils/common'
+import { client, getInfo } from '@/app/api/utils/common'
 
 export async function POST(request: NextRequest) {
   const body = await request.json()
   const {
     inputs,
     query,
-    response_mode: responseMode
   } = body
-  const { user } = getInfo(request);
-  const res = await client.createCompletionMessage(inputs, query, user, responseMode)
-  const stream = await OpenAIStream(res as any)
-  return new Response(stream as any)
+  const { user } = getInfo(request)
+  const res = await client.createCompletionMessage(inputs, query, user, true)
+  return new Response(res.data as any)
 }
