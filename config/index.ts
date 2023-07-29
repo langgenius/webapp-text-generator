087982f225b636f5237c68dcbeccc849a285589b
lz0423 <1248820667@qq.com>
2023-07-30 00:21:15 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,6 @@
 import type { AppInfo } from '@/types/app'
 export const APP_ID = 'aa570ffc-3223-46af-8133-b2e832d21e5f'
-export const API_KEY = ''
+export const API_KEY = 'd9bacf339597693db41b45a88a2dd996904008499329541541f53e3f288d382f'
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
   title: 'Text Generator APP',
