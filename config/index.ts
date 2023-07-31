327c881ec2d199b19ef3f1fde6feb8bc5e0bfac6
lz0423 <1248820667@qq.com>
2023-07-31 12:55:37 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,5 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = 'aa570ffc-3223-46af-8133-b2e832d21e5f'
+export const APP_ID = '86d2f021-c8ea-42e4-912a-969909044102'
 export const API_KEY = 'd9bacf339597693db41b45a88a2dd996904008499329541541f53e3f288d382f'
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
