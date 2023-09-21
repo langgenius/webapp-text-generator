6b8fc238e0e61eece029a60cbb774112f5d42aa2
Joel <iamjoel007@gmail.com>
2023-09-21 14:05:38 +0800

    fix: optional i18n missing

--- app/components/config-scence/index.tsx
+++ app/components/config-scence/index.tsx
@@ -54,7 +54,7 @@ const ConfigSence: FC<IConfigSenceProps> = ({
                   <input
                     type="text"
                     className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 "
-                    placeholder={`${item.name}${!item.required ? `(${t('appDebug.variableTable.optional')})` : ''}`}
+                    placeholder={`${item.name}${!item.required ? `(${t('app.common.optional')})` : ''}`}
                     value={inputs[item.key]}
                     onChange={(e) => { onInputsChange({ ...inputs, [item.key]: e.target.value }) }}
                     maxLength={item.max_length || DEFAULT_VALUE_MAX_LEN}
@@ -63,7 +63,7 @@ const ConfigSence: FC<IConfigSenceProps> = ({
                 {item.type === 'paragraph' && (
                   <textarea
                     className="block w-full h-[104px] p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 "
-                    placeholder={`${item.name}${!item.required ? `(${t('appDebug.variableTable.optional')})` : ''}`}
+                    placeholder={`${item.name}${!item.required ? `(${t('app.common.optional')})` : ''}`}
                     value={inputs[item.key]}
                     onChange={(e) => { onInputsChange({ ...inputs, [item.key]: e.target.value }) }}
                   />
