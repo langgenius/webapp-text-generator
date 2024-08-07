ffc025c1ae905fc3566f25cdfd4ba34b0af54f41
Joel <iamjoel007@gmail.com>
2024-08-07 16:51:54 +0800

    chore: not show workflow run detail (#29)

--- app/components/result/workflow/node.tsx
+++ app/components/result/workflow/node.tsx
@@ -3,11 +3,8 @@ import type { FC } from 'react'
 import { useEffect, useState } from 'react'
 import cn from 'classnames'
 import BlockIcon from './block-icon'
-import CodeEditor from './code-editor'
-import { CodeLanguage } from '@/types/app'
 import { AlertCircle, AlertTriangle } from '@/app/components/base/icons/line/alertsAndFeedback'
 import { CheckCircle, Loading02 } from '@/app/components/base/icons/line/general'
-import { ChevronRight } from '@/app/components/base/icons/line/arrows'
 import type { NodeTracing } from '@/types/app'
 
 type Props = {
@@ -50,12 +47,7 @@ const NodePanel: FC<Props> = ({ nodeInfo, hideInfo = false }) => {
           )}
           onClick={() => setCollapseState(!collapseState)}
         >
-          <ChevronRight
-            className={cn(
-              'shrink-0 w-3 h-3 mr-1 text-gray-400 transition-all group-hover:text-gray-500',
-              !collapseState && 'rotate-90',
-            )}
-          />
+
           <BlockIcon size={hideInfo ? 'xs' : 'sm'} className={cn('shrink-0 mr-2', hideInfo && '!mr-1')} type={nodeInfo.node_type} toolIcon={nodeInfo.extras?.icon || nodeInfo.extras} />
           <div className={cn(
             'grow text-gray-700 text-[13px] leading-[16px] font-semibold truncate',
@@ -80,48 +72,6 @@ const NodePanel: FC<Props> = ({ nodeInfo, hideInfo = false }) => {
             </div>
           )}
         </div>
-        {!collapseState && (
-          <div className='pb-2'>
-            <div className={cn('px-[10px] py-1', hideInfo && '!px-2 !py-0.5')}>
-              {nodeInfo.status === 'failed' && (
-                <div className='px-3 py-[10px] bg-[#fef3f2] rounded-lg border-[0.5px] border-[rbga(0,0,0,0.05)] text-xs leading-[18px] text-[#d92d20] shadow-xs'>{nodeInfo.error}</div>
-              )}
-            </div>
-            {nodeInfo.inputs && (
-              <div className={cn('px-[10px] py-1', hideInfo && '!px-2 !py-0.5')}>
-                <CodeEditor
-                  readOnly
-                  title={<div>INPUT</div>}
-                  language={CodeLanguage.json}
-                  value={nodeInfo.inputs}
-                  isJSONStringifyBeauty
-                />
-              </div>
-            )}
-            {nodeInfo.process_data && (
-              <div className={cn('px-[10px] py-1', hideInfo && '!px-2 !py-0.5')}>
-                <CodeEditor
-                  readOnly
-                  title={<div>PROCESS DATA</div>}
-                  language={CodeLanguage.json}
-                  value={nodeInfo.process_data}
-                  isJSONStringifyBeauty
-                />
-              </div>
-            )}
-            {nodeInfo.outputs && (
-              <div className={cn('px-[10px] py-1', hideInfo && '!px-2 !py-0.5')}>
-                <CodeEditor
-                  readOnly
-                  title={<div>OUTPUT</div>}
-                  language={CodeLanguage.json}
-                  value={nodeInfo.outputs}
-                  isJSONStringifyBeauty
-                />
-              </div>
-            )}
-          </div>
-        )}
       </div>
     </div>
   )
