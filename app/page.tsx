62c47b624acf72048a990e81d9fd2a54cc41f44d
brunoogp <147428822+brunoogp@users.noreply.github.com>
2025-02-05 23:42:43 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+"use client"; // Garante que este componente seja tratado no cliente
+import React from "react";
 
-const App = () => {
+export default function Page() {
   return (
     <div className="flex items-center justify-center h-screen text-white">
-      <h1 className="text-2xl font-bold">O layout está funcionando!</h1>
+      <h1 className="text-2xl font-bold">✅ O layout está funcionando corretamente!</h1>
+      <p className="text-gray-400">Se isso sumir, há um problema no carregamento do Next.js.</p>
     </div>
-  )
+  );
 }
-
-export default React.memo(App)
